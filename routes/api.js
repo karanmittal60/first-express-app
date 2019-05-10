49b831b64b3194a78b247bd818188eea06d4eb74
unknown <ravibasil.empirecitylabs.com>
2019-05-10 12:41:54 +0530

    mail send

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const Ninja = require('../models/ninja')
+const Ninja = require('../models/ninja');
+var nodemailer = require('nodemailer');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'chill.bill@gmail.com',
+        pass: 'chill#123'
+    }
+});
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'tahirdev.clix@gmail.com',
+//         pass: 'd3v@t4h1r'
+//     }
+// });
+
+var mailOptions = {
+    from: 'chill.bill@gmail.com',
+    to: 'mittal.karan60@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+
 
 
 // get a ninjas from db
 router.get('/ninjas', function (req, res, next) {
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log("+++++++++mail error++++++++++",error);
+        } else {
+            console.log('+=====================Email sent: ======================' + info.response);
+        }
+    });
     res.send({type: "GET"});
 
 });
